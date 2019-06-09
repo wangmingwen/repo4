6d10f5ed50c12fd7beb4e9e0f664111bf731ec04
wangmignwen <wangmignwen@163.com>
2019-06-09 20:19:25 +0800

    把项目提交到本地仓库

--- git_project/src/com/itheima/test/GitTest.java
+++ git_project/src/com/itheima/test/GitTest.java
@@ -4,4 +4,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is a test....");
     }
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
